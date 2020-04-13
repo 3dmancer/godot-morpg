5f1ea46cd7b65657905a4f4ca1600c1f4d4026d3
Fredrik Lindblom <3dmancer@gmail.com>
2020-04-13 08:30:13 +0200

    added asynchandler

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,75 +1,56 @@
 import ErrorResponse from "../utils/errorResponse.js"
+import asyncHandler from "../middleware/async.js"
 import User from "../models/User.js"
 
-export const getUsers = async (req, res, next) => {
-  try {
-    const users = await User.find()
-    res.status(200).json({ success: true, data: users })
-  } catch (error) {
-    next(error)
-  }
-}
+export const getUsers = asyncHandler(async (req, res, next) => {
+  const users = await User.find()
+  res.status(200).json({ success: true, data: users })
+})
 
-export const getUser = async (req, res, next) => {
-  try {
-    const user = await User.findById(req.params.id)
+export const getUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.params.id)
 
-    if (!user) {
-      return next(new ErrorResponse(`User '${req.params.id}' not found.`), 404)
-    }
-    res.status(200).json({ success: true, data: user })
-  } catch (error) {
-    next(error)
+  if (!user) {
+    return next(new ErrorResponse(`User '${req.params.id}' not found.`), 404)
   }
-}
+  res.status(200).json({ success: true, data: user })
+})
 
 // Create User
-export const createUser = async (req, res, next) => {
-  try {
-    const user = await User.create(req.body)
+export const createUser = asyncHandler(async (req, res, next) => {
+  const user = await User.create(req.body)
 
-    res.status(201).json({
-      sucess: true,
-      data: user,
-    })
-  } catch (error) {
-    next(error)
+  res.status(201).json({
+    sucess: true,
+    data: user,
+  })
+})
+
+export const updateUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  })
+
+  if (!user) {
+    return next(new ErrorResponse(`User '${req.params.id}' not found.`), 404)
   }
-}
 
-export const updateUser = async (req, res, next) => {
-  try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    })
+  res.status(200).json({
+    sucess: true,
+    data: user,
+  })
+})
 
-    if (!user) {
-      return next(new ErrorResponse(`User '${req.params.id}' not found.`), 404)
-    }
+export const deleteUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findByIdAndDelete(req.params.id)
 
-    res.status(200).json({
-      sucess: true,
-      data: user,
-    })
-  } catch (error) {
-    next(error)
+  if (!user) {
+    return next(new ErrorResponse(`User '${req.params.id}' not found.`), 404)
   }
-}
 
-export const deleteUser = async (req, res, next) => {
-  try {
-    const user = await User.findByIdAndDelete(req.params.id)
-
-    if (!user) {
-      return next(new ErrorResponse(`User '${req.params.id}' not found.`), 404)
-    }
-
-    res.status(200).json({
-      sucess: true,
-      data: {},
-    })
-  } catch (error) {
-    next(error)
-  }
-}
+  res.status(200).json({
+    sucess: true,
+    data: {},
+  })
+})
